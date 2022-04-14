cb4eab5e9ec3756d6b83b8179a632e5e359b977a
JT <wraith.twitch@gmail.com>
2022-04-13 22:37:55 -0400

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityModManagerNet;
+using HarmonyLib;
+
+namespace WraithMods
+{
+    public class Main
+    {
+        static class Main
+        {
+            public static bool Enabled;
+
+            static bool Load(UnityModManager.ModEntry modEntry)
+            {
+                var harmony = new Harmony(modEntry.Info.Id);
+                modEntry.OnToggle = OnToggle;
+                harmony.PatchAll();
+                return true;
+            }
+
+            static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+            {
+                Enabled = value;
+                return true;
+
+            }
+        }
+    }
+}
